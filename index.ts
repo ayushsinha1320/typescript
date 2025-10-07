f25a3e06f62b7e877842dafb00a4061aa89545b2
Ayush Sinha <ayushs@testvagrant.com>
2025-10-07 15:05:07 +0530

    Initial commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+// Types
+const my_name:string = "Ayush";
+const my_age:number = 23;
+
+console.log(`my_name = ${typeof(my_name)}`);
+console.log(`my_age = ${typeof(my_age)}`);
+
+// Union Types
+const my_name_age:string | number = 20;
+console.log(`my_name_age = ${typeof(my_name_age)}`);
